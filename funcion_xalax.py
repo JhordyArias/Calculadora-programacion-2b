0c6fdd94659f40fd711793f4d1d7d14fa37a26a0
Gersson Eduardo <gersson.mc@hotmail.com>
2016-07-06 20:00:52 -0500

    Funcion "X elevado a la ×"
    Esta funcion permite calcular el resultado de "X elevado a la x", donde: "X" es la base y "×" es el exponente.

---
+++ funcion_xalax.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import time
+
+def xalax():
+    os.system("cls")
+    print("Modelo: X^×\n*X: base.\n*×: exponente.")
+    print("\nIngrese valor de la base:")
+    while True:
+        try:
+            base = float(input())
+            break
+        except ValueError:
+            print("BASE Ingresado no valido, intente nuevamente...")
+    print("\nIngrese valor del exponente:")
+    while True:
+        try:
+            exp = float(input())
+            break
+        except ValueError:
+            print("EXPONENTE Ingresado no valido, intente nuevamente...")
+    print()
+    print(base ** exp)
+
+xalax()
