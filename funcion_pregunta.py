018ba715af61a5720ef8ea0f877cb701e5f0eb87
Gersson Eduardo <gersson.mc@hotmail.com>
2016-07-06 20:16:17 -0500

    Funcion "Pregunta"
    Esta función pregunta si deseamos realizar otra operación o no, y de acuerdo a la respuesta regresa al menú o cierra el programa.

---
+++ funcion_pregunta.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import time
+
+def menu():
+    print("--- MENU ---")
+    print("1.- Suma")
+    print("2.- Resta")
+    print("3.- Salir")
+
+def pregunta():
+    print("\n¿Desea realizar otra operacion?")
+    while True:
+        print("Si / No:")
+        respuesta = input()
+        if(respuesta.isalpha() == True and respuesta.lower() == "si" or respuesta.lower() == "no"):
+            respuesta = respuesta.lower()
+            break
+        else:
+            print("Opps! No era valido, intente nuevamente...")
+    if(respuesta == "si"):
+        os.system("cls")
+        menu()
+    if(respuesta == "no"):
+        os.system("cls")
+        print("Cerrando programa...")
+        time.sleep(2)
+        sys.exit(1)
+
+pregunta()
